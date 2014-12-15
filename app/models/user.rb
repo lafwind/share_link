bafb32f7173bbcc9c265ba4ab0bb8a4430090fec
Lafwind <lafwind@gmail.com>
2014-12-15 17:34:18 +0800

    为User model添加安全密码

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,8 @@ class User < ActiveRecord::Base
   validates :email, presence: true, length: { maximum: 50 },
     format: { with: VALID_EMAIL_REGEX },
     uniqueness: { case_sensitive: false }
+
+  validates :password, length: { minimum: 6 }
+
+  has_secure_password
 end
