896077cd96a392a94093abb66b3edb361a4d02d9
Lafwind <lafwind@gmail.com>
2014-12-17 20:40:26 +0800

    完成利用session的登入

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
 
-  get 'sessions/new'
 
   root 'static_pages#home'
   get 'about' => 'static_pages#about'
   get 'signup' => 'users#new'
   get 'login' => 'sessions#new'
-  post 'login' => 'session#create'
+  post 'login' => 'sessions#create'
   delete 'logout' => 'sessions#destroy'
   resources :users
 
