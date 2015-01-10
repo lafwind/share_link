2f31540d8225eb5eec4af1cb9908786b6aa492df
Lafwind <lafwind@gmail.com>
2015-01-10 20:30:42 +0800

    完成创建新链接

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -5,4 +5,23 @@ class LinksController < ApplicationController
     @links = @user.links.all
   end
 
+  def new
+    @user = User.find(params[:user_id])
+    @link = @user.links.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @link = @user.links.create(params[:link].permit(:title, :url))
+
+    respond_to do |format|
+      if @link.save
+        flash[:success] = "Create new link successfully!"
+        format.html { redirect_to user_links_path(@user) }
+      else
+        flash[:error] = "Some error!"
+        format.html { render 'new' }
+      end
+    end
+  end
 end
