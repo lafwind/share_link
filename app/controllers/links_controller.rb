712de4201c6eb5ecd199bcd6e7df5774e1ed9216
Lafwind <lafwind@gmail.com>
2015-01-10 21:42:46 +0800

    完成编辑链接功能

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -25,10 +25,30 @@ class LinksController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:user_id])
+    @link = @user.links.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:user_id])
+    @link = @user.links.find(params[:id])
+    respond_to do |format|
+      if @link.update_attributes(params[:link].permit(:title, :url))
+        flash[:success] = "Update successfully!"
+        format.html { redirect_to user_links_path(@user) }
+      else
+        flash[:error] = "Some error here!"
+        format.html { render 'edit' }
+      end
+    end
+  end
+
   def destroy
     @user = User.find(params[:user_id])
     @link = @user.links.find(params[:id])
     @link.destroy
     redirect_to user_links_path
   end
+
 end
