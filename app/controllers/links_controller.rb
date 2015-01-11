e39a45835be1aaf446b8303df265a72ae6480d27
Lafwind <lafwind@gmail.com>
2015-01-11 18:42:24 +0800

    增加在新增链接的验证

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,53 +2,77 @@ class LinksController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @links = @user.links.all
+    if @user != current_user
+      redirect_to root_url
+    else
+      @links = @user.links.all
+    end
   end
 
   def new
     @user = User.find(params[:user_id])
-    @link = @user.links.new
+    if @user != current_user
+      redirect_to root_url
+    else
+      @link = @user.links.new
+    end
   end
 
   def create
     @user = User.find(params[:user_id])
-    @link = @user.links.create(params[:link].permit(:title, :url))
+    if @user != current_user
+      redirect_to root_url
+    else
+      @link = @user.links.create(params[:link].permit(:title, :url))
 
-    respond_to do |format|
-      if @link.save
-        flash[:success] = "Create new link successfully!"
-        format.html { redirect_to user_links_path(@user) }
-      else
-        flash[:error] = "Some error!"
-        format.html { render 'new' }
+      respond_to do |format|
+        if @link.save
+          flash[:success] = "Create new link successfully!"
+          format.html { redirect_to user_links_path(@user) }
+        else
+          flash[:error] = "Some error!"
+          format.html { render 'new' }
+        end
       end
     end
   end
 
   def edit
     @user = User.find(params[:user_id])
-    @link = @user.links.find(params[:id])
+    if @user != current_user
+      redirect_to root_url
+    else
+      @link = @user.links.find(params[:id])
+    end
   end
 
   def update
     @user = User.find(params[:user_id])
-    @link = @user.links.find(params[:id])
-    respond_to do |format|
-      if @link.update_attributes(params[:link].permit(:title, :url))
-        flash[:success] = "Update successfully!"
-        format.html { redirect_to user_links_path(@user) }
-      else
-        flash[:error] = "Some error here!"
-        format.html { render 'edit' }
+    if @user != current_user
+      redirect_to root_url
+    else
+      @link = @user.links.find(params[:id])
+      respond_to do |format|
+        if @link.update_attributes(params[:link].permit(:title, :url))
+          flash[:success] = "Update successfully!"
+          format.html { redirect_to user_links_path(@user) }
+        else
+          flash[:error] = "Some error here!"
+          format.html { render 'edit' }
+        end
       end
     end
   end
 
   def destroy
     @user = User.find(params[:user_id])
-    @link = @user.links.find(params[:id])
-    @link.destroy
-    redirect_to user_links_path
+    if @user != current_user
+      redirect_to root_url
+    else
+      @link = @user.links.find(params[:id])
+      @link.destroy
+      redirect_to user_links_path
+    end
   end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -20,6 +20,21 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_url
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to root_url
+    else
+    end
+  end
+
   private
 
     def user_params
