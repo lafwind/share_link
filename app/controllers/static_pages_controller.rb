96809ae391df84741b1e3c01ab096d31a1f275c6
Lafwind <lafwind@gmail.com>
2015-01-13 23:27:56 +0800

    修复在主页每个user只能显示一个share的link

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,13 @@
 class StaticPagesController < ApplicationController
   def home
     @users = User.all
-    @all_share_links = {}
+    @all_share_links = []
 
     @users.each do |user|
       user_links = user.links.all
       user_links.each do |link|
         if link.sharing == true
-          @all_share_links[user] = link
+          @all_share_links << [user, link]
         end
       end
     end
