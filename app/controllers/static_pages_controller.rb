ee169c926661629864c6dd38093f3985765bf317
Lafwind <lafwind@gmail.com>
2015-05-10 22:02:26 +0800

    discover可根据选择按"hottest"或"newest"进行排序

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,17 @@
 class StaticPagesController < ApplicationController
   def home
+    @ranked_by = [
+      {name: "hottest"}, {name: "newest"}
+    ]
+
     @users = User.all
     # @all_share_links = []
 
-    @links = Link.where(user_id: @users, sharing: true).order("like_count DESC").order("created_at DESC").paginate(page: params[:page], per_page: 10)
+    if params[:ranked_by].blank? || params[:ranked_by] == "hottest"
+      @links = Link.where(user_id: @users, sharing: true).order("like_count DESC").order("created_at DESC").paginate(page: params[:page], per_page: 10)
+    elsif params[:ranked_by] = "newest"
+      @links = Link.where(user_id: @users, sharing: true).order("created_at DESC").paginate(page: params[:page], per_page: 10)
+    end
     # @users.each do |user|
     #   user_links = user.links.all
     #   user_links.each do |link|
